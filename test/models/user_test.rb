3e4470bfad261743656eeb5630970285390b9159
taka.usu <liverpenguin1408@gmail.com>
2020-04-15 09:11:10 +0000

    ユーザー名、メールアドレスの長さについて、バリデーションを記載

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -23,4 +23,15 @@ class UserTest < ActiveSupport::TestCase
     # ユーザーが登録されない
     assert_not @user.valid?
   end
+  
+  test "name should not be too long" do
+    # 名前を51文字にする
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+  
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 end
