e060afb1f20b9f0882de786f7bda165b4dfb5a90
taka.usu <liverpenguin1408@gmail.com>
2020-04-19 09:05:25 +0000

    一時セッションを保存する機能を作成

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+  # 渡されたユーザーでログイン
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  # 現在のユーザー情報を取得
+  def current_user
+    if session[:uesr_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
