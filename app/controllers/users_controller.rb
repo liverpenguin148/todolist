a28dfb52829ef261a335848f8dbf1568238f6217
taka.usu <liverpenguin1408@gmail.com>
2020-04-26 04:46:07 +0000

    ユーザー編集ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,10 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   private
     # Strong Parameter permitで指定した属性以外、許可しない
     def user_params
