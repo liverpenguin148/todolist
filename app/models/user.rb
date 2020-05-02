532d07b41740a7bbf3b38d39ed6470fe70b3a6b2
taka.usu <liverpenguin1408@gmail.com>
2020-05-02 14:01:45 +0000

    アカウントの有効化データモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # remember_tokenという仮属性の作成
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -11,7 +12,9 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6}, allow_nil: true
   
+  # クラスメソッドの定義
   class << self
+    # 渡された文字列をハッシュ化
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                     BCrypt::Engine.cost
@@ -23,7 +26,8 @@ class User < ApplicationRecord
       SecureRandom.urlsafe_base64
     end
   end
-    
+  
+  # インスタンスメソッドの定義
   # 記憶トークンをハッシュ化(remember_digest)し、DBに保存
   def remember
     self.remember_token = User.new_token
@@ -40,4 +44,14 @@ class User < ApplicationRecord
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+  
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
+    
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
